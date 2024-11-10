e2f7be82ccfbc70ef07a3eedc24fc49946c9f228
Daria Dmiti <dmatrievadasha@gmail.com>
2024-11-10 20:14:42 +0300

    creating recipe step by step

--- handlers.py
+++ handlers.py
@@ -14,6 +14,9 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 
+telegram_session = {}
+
+
 # users = session.query(User).all()
 # for user in users:
 #     print(f"ID: {user.id}, Name: {user.name}, Age: {user.age}, Email: {user.email}")
@@ -22,27 +25,54 @@ session = Session()
 @bot.message_handler(commands=['help', 'start']) 
 @message_auth
 def send_welcome(message):
-    bot.reply_to(message, 'Hi there, I am EchoBot. I am here to echo your kind words back to you. Just say anything nice and I\'ll say the exact same thing to you!')
+    bot.send_message(message.chat.id, 'Привет, мир!')
 
 
-@bot.message_handler(regexp='#рецепт|#Рецепт|#recipe|#Recipe')
+@bot.message_handler(commands=['add_recipe']) 
 @message_auth
-def echo_message(message):
-    markup = t.types.InlineKeyboardMarkup()
-    callback_data = f'save_recipe,{message.message_id}'
-    save_btn = t.types.InlineKeyboardButton('Сохранить', callback_data=callback_data)
-    markup.add(save_btn)
-    bot.reply_to(message, 'Заебись рецепт! Сохранить?', reply_markup=markup)
+def ask_recipe_name(message):
+    telegram_session[message.from_user.id] = {'add_recipe': {}}
+    bot.send_message(message.chat.id, 'Отлично! Какое название?')
+
+@bot.message_handler()
+@message_auth
+def handle_message(message):
+    user_session = telegram_session[message.from_user.id]
+    if 'add_recipe' in user_session:
+        if 'name' not in user_session['add_recipe']:
+            user_session['add_recipe']['name'] = message.text
+            bot.send_message(message.chat.id, 'Отлично! Какие ингредиенты?')
+        elif 'ingredients' not in user_session['add_recipe']:
+            user_session['add_recipe']['ingredients'] = message.text
+            bot.send_message(message.chat.id, 'Отлично! Добавь описание')
+        elif 'description' not in user_session['add_recipe']:
+            user_session['add_recipe']['description'] = message.text
+            recipe_data = user_session['add_recipe']
+            text = '\n\n'.join(
+                (
+                    recipe_data['name'],
+                    recipe_data['ingredients'],
+                    recipe_data['description']
+                )
+            )
+            markup = t.types.InlineKeyboardMarkup()
+            save_btn = t.types.InlineKeyboardButton(
+                'Сохранить', callback_data='save_recipe'
+            )
+            markup.add(save_btn)
+            bot.send_message(message.chat.id, text, reply_markup=markup)
 
 
-@bot.callback_query_handler(func=lambda callback: callback.data.split(',')[0] == 'save_recipe')
+
+@bot.callback_query_handler(func=lambda callback: callback.data == 'save_recipe')
 def handle_save_recipe_query(callback):
-    new_recipe = Recipe(message_id = callback.data.split(',')[1])
-    session.add(new_recipe)
-    session.commit()
+    recipe_data = telegram_session[callback.from_user.id]['add_recipe']
+#   new_recipe = Recipe(message_id=callback.data.split(',')[1])
+#   session.add(new_recipe)
+#   session.commit()
 
     chat_id = callback.message.chat.id
-    bot.edit_message_text('Заебись рецепт! Сохранить?', chat_id, callback.message.id)
+    # bot.edit_message_text('Заебись рецепт! Сохранить?', chat_id, callback.message.id)
     bot.send_message(chat_id, "Сохранено!")
 
 
