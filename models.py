25726e62c68545e4c0da2f3ffd44c611aaf0fc0e
Daria Dmiti <dmatrievadasha@gmail.com>
2024-11-10 20:17:59 +0300

    add name, ingredients and description fields for recipes

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer
+from sqlalchemy import create_engine, Column, Integer, String, Text
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -7,7 +7,8 @@ class Recipe(Base):
     __tablename__ = 'recipes'
     
     id = Column(Integer, primary_key=True)
-    message_id = Column(Integer, unique=True, nullable=False)
+    name_test = Column(String, nullable=False)
+    ingredients = Column(Text, nullable=False)
+    description = Column(Text, nullable=False)
 
 engine = create_engine('sqlite:///pokarm.db')
-Base.metadata.create_all(engine)
