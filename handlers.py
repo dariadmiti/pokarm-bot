99c3fe53ca2950c8b3ba045d942ab3be00cd9afa
Daria Dmiti <dmatrievadasha@gmail.com>
2025-01-05 16:53:38 +0300

    get recipes by ingredient

--- handlers.py
+++ handlers.py
@@ -43,6 +43,13 @@ def get_random_recipe(message):
     text = '\n\n'.join((recipe.name, recipe.ingredients, recipe.description))
     bot.send_message(message.chat.id, text)
 
+@bot.message_handler(commands=['find_ingredient'])
+@message_auth
+def find_ingredient(message):
+    telegram_session[message.from_user.id] = {'find_ingredient': {}}
+    bot.send_message(message.chat.id, 'Какой ингредиент ищешь?')
+
+
 
 @bot.message_handler()
 @message_auth
@@ -72,6 +79,22 @@ def handle_message(message):
             markup.add(save_btn)
             bot.send_message(message.chat.id, text, reply_markup=markup)
 
+    elif 'find_ingredient' in user_session:
+        search_string = message.text
+
+        recipes = db_session.query(Recipe).filter(
+            Recipe.ingredients.contains(search_string)
+        ).all()
+
+        markup = t.types.InlineKeyboardMarkup()
+        for recipe in recipes:
+            recipe_btn = t.types.InlineKeyboardButton(
+                recipe.name, callback_data=f'get_recipe,{recipe.id}'
+            )
+            markup.add(recipe_btn)
+
+        bot.send_message(message.chat.id, 'Результаты поиска:', reply_markup=markup)
+
 
 
 @bot.callback_query_handler(func=lambda callback: callback.data == 'save_recipe')
@@ -83,3 +106,9 @@ def handle_save_recipe_query(callback):
 
     bot.send_message(callback.message.chat.id, 'Сохранено!')
 
+@bot.callback_query_handler(func=lambda callback: callback.data.split(',')[0] == 'get_recipe')
+def handle_get_recipe_query(callback):
+    recipe_id = callback.data.split(',')[1]
+    recipe = db_session.query(Recipe).get(recipe_id)
+    text = '\n\n'.join((recipe.name, recipe.ingredients, recipe.description))
+    bot.send_message(callback.message.chat.id, text)
